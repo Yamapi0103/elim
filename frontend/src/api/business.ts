5d5fab642efb73435c567c9f39163cc12e71e6c1
joshua yeh <yamapi0103@gmail.com>
2024-03-31 23:03:06 +0800

    getlist

--- frontend/src/api/business.ts
+++ frontend/src/api/business.ts
@@ -1,3 +1,5 @@
 import { api } from 'src/boot/axios';
 
+export const getList = () => api.get('business/list');
+
 export const getBusiness = (id: number) => api.get(`business/${id}`);
--- frontend/src/pages/SalaryPage.vue
+++ frontend/src/pages/SalaryPage.vue
@@ -1,25 +1,69 @@
 <template>
   <q-page class="">
     <div>
-      <div>Salary page</div>
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      <div>薪資結帳單列表</div>
+      <q-table :rows="list" :columns="columns" />
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { getBusiness } from '../api/request';
+import { onBeforeMount, ref } from 'vue';
+import { getList } from '../api/business';
 defineOptions({
   name: 'SalaryPage',
 });
 
-getBusiness(6);
+const list = ref([]);
+
+onBeforeMount(async () => {
+  const { data } = await getList();
+  list.value = data;
+});
+
+const columns = [
+  {
+    label: '日期',
+    field: 'date',
+    name: 'date',
+  },
+  {
+    label: '車號',
+    field: 'carNo',
+    name: 'carNo',
+  },
+  {
+    label: '行程',
+    field: 'route',
+    name: 'route',
+  },
+  {
+    label: '車資',
+    field: 'fare',
+    name: 'fare',
+  },
+  {
+    label: '外收現金',
+    field: 'extraCash',
+    name: 'extraCash',
+  },
+  // {
+
+  // },
+  {
+    label: '小費',
+    field: 'tip',
+    name: 'tip',
+  },
+  {
+    label: '稅金',
+    field: 'taxes',
+    name: 'taxes',
+  },
+  {
+    label: '調車人',
+    field: 'orderer',
+    name: 'orderer',
+  },
+];
 </script>
