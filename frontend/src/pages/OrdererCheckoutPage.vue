fc668fa2f388adbd9571798289cbfd0264ec9ee2
joshua yeh <yamapi0103@gmail.com>
2024-05-01 18:58:47 +0800

    車用結帳單excel upload

---
+++ frontend/src/pages/OrdererCheckoutPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-uploader
+      label="上傳 Excel 文件"
+      accept=".xlsx, .xls"
+      :factory="fileUploaded"
+    />
+  </q-page>
+</template>
+
+<script setup>
+import { api } from 'src/boot/axios';
+
+const fileUploaded = (files) => {
+  const file = files[0];
+  const formData = new FormData();
+  formData.append('excel', file);
+  api.post('excel/import', formData);
+};
+</script>
